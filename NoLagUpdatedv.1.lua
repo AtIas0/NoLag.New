adfa9314448968ccecc199118df34efc30e50ba8
Atlas <jawalapsaw123@gmail.com>
2025-06-19 08:53:59 +0800

    Update NoLagUpdatedv.1.lua

--- NoLagUpdatedv.1.lua
+++ NoLagUpdatedv.1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AtIas0/NoLag.New/refs/heads/main/NoLagUpdated.lua"))()
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AhmaclV99/Speed-Hub-X/refs/heads/main/source2"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AtIas0/NoLag.New/refs/heads/main/NoLagUpdated.lua"))()
